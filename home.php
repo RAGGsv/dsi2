2079a5dfc1f26544b4c9f1378d1df3a855a7c512
Ronald <ragcorp.g@outlook.com>
2022-11-06 12:24:33 -0600

    subida

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
  <div class="col-md-12">
     <div class="panel">
       <div class="jumbotron text-center">
-         <h1>Esta es su nueva página de inicio</h1>
+         <h1>Bienvenidos al sistema de control de Bodega</h1>
      
       </div>
     </div>
